fab3552b131278c4f4a1634a91f3ad854b74f1bd
Hitrel <hitrel@outlook.com>
2020-05-16 19:01:07 +0800

    Add Camera Following and Zooming

---
+++ Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+    Camera camera = null;
+
+    Vector3 offset;
+
+    float initCameraSize = 0f;
+    float delta = 50f;
+
+    private void Start() {
+        offset = transform.position - target.position;
+        camera = GetComponent<Camera>();
+        initCameraSize = camera.orthographicSize;
+        delta += camera.orthographicSize;
+    }
+
+    private void FixedUpdate() {
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Tab)) {
+            ZoomOut();
+        } else {
+            ZoomIn();
+        }
+    }
+
+    private void ZoomOut() {
+        camera.orthographicSize = delta;
+    }
+
+    private void ZoomIn() {
+        camera.orthographicSize = initCameraSize;
+    }
+}
